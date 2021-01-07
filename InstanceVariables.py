a8ba7f0cd2ac588789eacf92c14f701fd79ac335
Yamuna Rao <63515445+yrao104@users.noreply.github.com>
2021-01-06 16:33:12 -0800

    Rename InstanceVariables.py to Week2/InstanceVariables.py

--- InstanceVariables.py
+++
@@ -1,2 +0,0 @@
-#instance variable - data held by an object
-#instance variables are not shared by all isntances of a class - they are variables that are specific to the object that they are attached to
---
+++ Week2/InstanceVariables.py
@@ -0,0 +1,2 @@
+#instance variable - data held by an object
+#instance variables are not shared by all isntances of a class - they are variables that are specific to the object that they are attached to
